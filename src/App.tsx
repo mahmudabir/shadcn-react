c03ae2bd77e5494e80997aa85c182611c485bb97
Abir Mahmud <amabirmahmud@gmail.com>
2025-07-14 00:21:39 +0600

    shadcn init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "./components/theme-provider"
+import { ModeToggle } from "./components/mode-toggle"
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle/>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
