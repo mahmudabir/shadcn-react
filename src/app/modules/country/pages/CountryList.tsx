e8aeadc126f2a5bfadb619f69a7952378f8434f8
Abir <amabirmahmud@gmail.com>
2025-07-14 12:44:52 +0600

    Update CountryList.tsx

--- src/app/modules/country/pages/CountryList.tsx
+++ src/app/modules/country/pages/CountryList.tsx
@@ -4,7 +4,7 @@ import { deleteCountry, getCountries } from '../api/countries.ts';
 import type { Country } from '../models/country.ts';
 import { COUNTRY_ROUTES } from "@/app/modules/country/routes/country-route-elements.ts";
 
-const CountryList= () => {
+const CountryList = () => {
   const [countries, setCountries] = useState<Country[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
@@ -47,17 +47,17 @@ const CountryList= () => {
     <div>
       <h1>Countries</h1>
       <ul>
-        {!countries?.length 
-        ? <h5>No countries found</h5>
-        : countries.map(country => (
-          <li key={country.id}>
-            <Link to={COUNTRY_ROUTES.details(country.id.toString())}>{country.nameEn}</Link>
-            {' | '}
-            <Link to={COUNTRY_ROUTES.edit(country.id.toString())}>Edit</Link>
-            {' | '}
-            <button onClick={() => handleDelete(country.id.toString())} style={{ color: 'red' }}>Delete</button>
-          </li>
-        ))}
+        {!countries?.length
+          ? <h5>No countries found</h5>
+          : countries.map(country => (
+            <li key={country.id}>
+              <Link to={COUNTRY_ROUTES.details(country.id.toString())}>{country.nameEn}</Link>
+              {' | '}
+              <Link to={COUNTRY_ROUTES.edit(country.id.toString())}>Edit</Link>
+              {' | '}
+              <button onClick={() => handleDelete(country.id.toString())} style={{ color: 'red' }}>Delete</button>
+            </li>
+          ))}
       </ul>
       <Link to={COUNTRY_ROUTES.create()}>Create New Country</Link>
     </div>
