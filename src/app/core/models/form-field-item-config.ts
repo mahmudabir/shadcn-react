ea7706b4c22b06dfe38c43c082695702286b3f2c
Abir <amabirmahmud@gmail.com>
2025-08-04 19:07:33 +0600

    form render select item options passing fix

--- src/app/core/models/form-field-item-config.ts
+++ src/app/core/models/form-field-item-config.ts
@@ -10,5 +10,6 @@ export type FormFieldItemConfig<T> = {
     label: string,
     description: string,
     placeholder?: string,
-    className?: "w-full" | "w-1/2" | "w-1/3" | "w-1/4" | "w-1/5" | "w-1/6" | "w-1/12" | "w-full md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5"
+    className?: "w-full" | "w-1/2" | "w-1/3" | "w-1/4" | "w-1/5" | "w-1/6" | "w-1/12" | "w-full md:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5",
+    options?: SelectOption[],
 };
--- src/app/modules/city/components/CityForm.tsx
+++ src/app/modules/city/components/CityForm.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useRef, useState } from "react"
 import { useForm } from "react-hook-form"
 import { confirmPopup } from "../../../../components/custom/confirmation-popup"
 import { FormFieldItem, FormFieldItems, SelectFieldItem } from "../../../../components/custom/form-field-item"
@@ -33,12 +33,8 @@ const CityForm = ({ initialData = new City(), onSubmit, submitLabel = 'Submit' }
         logFormValues(formValues);
     }, [formValues, form.formState.errors]); // Run when values or errors change
 
-    const [countryOptions, setCountryOptions] = useState<SelectOption[]>([]);
-
     useEffect(() => {
-        countryViewModel.getSelectItems("nameEn", "id", "Select a country").then(() => {
-            setCountryOptions(countryViewModel.selectItems);
-        });
+        countryViewModel.getSelectItems("nameEn", "id", "Select a country");
     }, []);
 
     const confirmationOptions = {
@@ -63,7 +59,8 @@ const CityForm = ({ initialData = new City(), onSubmit, submitLabel = 'Submit' }
             label: "Country",
             description: "Select the country for this city",
             placeholder: "Select a country",
-            className: "w-full"
+            className: "w-full",
+            options: countryViewModel.selectItems
         },
         {
             type: "text",
@@ -99,9 +96,6 @@ const CityForm = ({ initialData = new City(), onSubmit, submitLabel = 'Submit' }
         }
     ];
 
-    console.log(countryOptions);
-
-
     return (
         <Form {...form}>
             <form className="w-full mx-auto p-8 rounded-xl shadow-xl space-y-8"
@@ -122,7 +116,7 @@ const CityForm = ({ initialData = new City(), onSubmit, submitLabel = 'Submit' }
                 {/* Countries Section */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
                     {renderFormFieldsUsingFormFieldItemConfig
-                        ? <FormFieldItems items={cityFieldsConfig} options={countryViewModel.selectItems} />
+                        ? <FormFieldItems items={cityFieldsConfig} />
                         : (
                             <>
                                 <SelectFieldItem
