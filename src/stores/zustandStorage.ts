64f0beacd15ed7d90cfdc9768179cecea6e2a9eb
Abir <amabirmahmud@gmail.com>
2025-08-18 11:50:39 +0600

    Update zustandStorage.ts

--- src/stores/zustandStorage.ts
+++ src/stores/zustandStorage.ts
@@ -1,4 +1,4 @@
-﻿import { get, set, del } from 'idb-keyval';
+﻿import * as idbKeyVal from 'idb-keyval';
 import { createJSONStorage, StateStorage } from 'zustand/middleware';
 import { QUERY_STALE_TIME_MS } from '@/lib/utils.ts';
 
@@ -9,46 +9,48 @@ type StoredData = {
 
 const simpleIdbStorage: StateStorage = {
   getItem: async (name) => {
-    const data = await get(name);
+    const data = await idbKeyVal.get(name);
     return data ? JSON.stringify(data) : null;
   },
   setItem: async (name, value) => {
-    await set(name, JSON.parse(value));
+    await idbKeyVal.set(name, JSON.parse(value));
   },
   removeItem: async (name) => {
-    await del(name);
+    await idbKeyVal.del(name);
   },
 };
 
-const idbStore: StateStorage = {
-  getItem: async (name) => {
-    const record = await get<StoredData>(name);
-    if (!record) return null;
+const idbStore = (expiryMs: number = 0): StateStorage => {
+  return {
+    getItem: async (name) => {
+      const record = await idbKeyVal.get<StoredData>(name);
+      if (!record) return null;
 
-    const now = Date.now();
+      const now = Date.now();
 
-    // Check expiry
-    if (record.expiry && now > record.expiry) {
-      await del(name);
-      return null; // Treat as no data
-    }
+      // Check expiry
+      if (record.expiry && now > record.expiry) {
+        await idbKeyVal.del(name);
+        return null; // Treat as no data
+      }
 
-    return JSON.stringify(record.value);
-  },
+      return JSON.stringify(record.value);
+    },
 
-  setItem: async (name, value) => {
-    await set(name, {
-      value: JSON.parse(value),
-      expiry: Date.now() + QUERY_STALE_TIME_MS,
-    });
-  },
+    setItem: async (name, value) => {
+      await idbKeyVal.set(name, {
+        value: JSON.parse(value),
+        expiry: Date.now() + expiryMs,
+      });
+    },
 
-  removeItem: async (name) => {
-    await del(name);
-  },
+    removeItem: async (name) => {
+      await idbKeyVal.del(name);
+    },
+  }
 };
 
-const createStateStorage = (storage: Storage): StateStorage => {
+const createStateStorage = (storage: Storage, expiryMs: number = 0): StateStorage => {
   return {
     getItem: async (name) => {
       const record = JSON.parse(storage.getItem(name)) as StoredData;
@@ -68,7 +70,7 @@ const createStateStorage = (storage: Storage): StateStorage => {
     setItem: async (name, value) => {
       const data: StoredData = {
         value: JSON.parse(value),
-        expiry: Date.now() + QUERY_STALE_TIME_MS,
+        expiry: Date.now() + expiryMs,
       };
       storage.setItem(name, JSON.stringify(data));
     },
@@ -80,5 +82,5 @@ const createStateStorage = (storage: Storage): StateStorage => {
 };
 
 
-export const zustandIndexedDBStorage = () => createJSONStorage(() => idbStore);
-export const zustandStateStorage = (storage: Storage) => createJSONStorage(() => createStateStorage((storage)));
+export const zustandIndexedDBStorage = (expiryMs: number = QUERY_STALE_TIME_MS) => createJSONStorage(() => idbStore(expiryMs));
+export const zustandStateStorage = (storage: Storage, expiryMs: number = QUERY_STALE_TIME_MS) => createJSONStorage(() => createStateStorage(storage, expiryMs));
